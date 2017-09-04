2e4351c6ac7b21be10c6a6cd0da3f746eb9bae7a
snowcxt <snowcxt@gmail.com>
2017-09-03 17:33:34 -0700

    create Expr.g4

---
+++ index.js
@@ -0,0 +1,14 @@
+var antlr4 = require('antlr4/index');
+var ExprLexer = require.main.require('./parsers/expr/ExprLexer');
+var ExprParser = require.main.require('./parsers/expr/ExprParser');
+
+var input = "5*(0+2)\r\n";
+var chars = new antlr4.InputStream(input);
+var lexer = new ExprLexer.ExprLexer(chars);
+var tokens  = new antlr4.CommonTokenStream(lexer);
+var parser = new ExprParser.ExprParser(tokens);
+parser.buildParseTrees = true;
+var tree = parser.prog();
+
+console.log(`parsed successful:\r\n${input}`);
+// console.log(tokens)

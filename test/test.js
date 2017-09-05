e23dd8b4cde450f6a08eb5ba6d9dd7811ee7af33
snowcxt <snowcxt@gmail.com>
2017-09-05 12:40:24 -0700

    add auto test

---
+++ test/test.js
@@ -0,0 +1,29 @@
+import assert from 'assert';
+import run from '../dist/run';
+
+describe("arithmetic", function () {
+    it("simple", function () {
+        assert.equal(3, run('1+2;'));
+    });
+    it("multiple & sum", function () {
+        assert.equal(7, run('1+2*3;'));
+    });
+    it("paren & multiple & sum", function () {
+        assert.equal(9, run('(1+2)*3;'));
+    });
+    it("paren x 2 & multiple & sum", function () {
+        assert.equal(27, run('(1+2 * (9-5))*3;'));
+    });
+});
+
+describe("assign", function () {
+    it("1 variable", function () {
+        assert.equal(1, run('a=1;a;'));
+    });
+    it("2 variables", function () {
+        assert.equal(-1, run('a=1;b=2;a-b;'));
+    });
+    it("nested variables", function () {
+        assert.equal(12, run('a=1;b=2;c=a+b;d=c-(a*b);(c+d)*3;'));
+    });
+});

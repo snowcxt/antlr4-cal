1fbcac5ef4516fa84dc21876ab23dc067c9bd67f
snowcxt <snowcxt@gmail.com>
2017-09-05 22:14:29 -0700

    editor show error on update

--- editor/index.js
+++ editor/index.js
@@ -3,11 +3,11 @@ var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
 editor.getSession().setMode("ace/mode/my-mode");
 
-setTimeout(function () {
+editor.getSession().on('change', function () {
     editor.getSession().setAnnotations([{
         row: 0,
         column: 2,
         text: "Hier stimmt was nicht",
         type: "error" // also warning and information
     }]);
-}, 1000);
+});

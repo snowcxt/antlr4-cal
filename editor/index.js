763ada820af62c1ce037da155a2d88835a3af935
snowcxt <snowcxt@gmail.com>
2017-09-05 22:12:16 -0700

    clean editor code

--- editor/index.js
+++ editor/index.js
@@ -1,3 +1,13 @@
-var editor = ace.edit('editor');
+
+var editor = ace.edit("editor");
 editor.setTheme("ace/theme/twilight");
-editor.getSession().setMode('ace/mode/my-mode');
+editor.getSession().setMode("ace/mode/my-mode");
+
+setTimeout(function () {
+    editor.getSession().setAnnotations([{
+        row: 0,
+        column: 2,
+        text: "Hier stimmt was nicht",
+        type: "error" // also warning and information
+    }]);
+}, 1000);

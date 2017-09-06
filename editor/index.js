05ed524727d222001b8a429b71f50240383d9fa9
snowcxt <snowcxt@gmail.com>
2017-09-06 00:30:02 -0700

    editor show lint error

--- editor/index.js
+++ editor/index.js
@@ -1,13 +1,14 @@
 
 var editor = ace.edit("editor");
-editor.setTheme("ace/theme/twilight");
+editor.setTheme("ace/theme/chrome");
 editor.getSession().setMode("ace/mode/my-mode");
-
+var Range = ace.require('ace/range').Range;
 editor.getSession().on('change', function () {
     editor.getSession().setAnnotations([{
-        row: 0,
+        row: 2,
         column: 2,
         text: "Hier stimmt was nicht",
         type: "error" // also warning and information
     }]);
+    editor.session.addMarker(new Range(2, 2, 2, 3), "lint-mark-error", false);
 });

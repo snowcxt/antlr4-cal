debd060f99b4c5426601296f81be2ad2822cd410
snowcxt <snowcxt@gmail.com>
2017-09-06 17:17:16 -0700

    test invalid chars

--- test/validate.js
+++ test/validate.js
@@ -9,4 +9,25 @@ describe("validation", function () {
     it("simple", function () {
         assert.deepEqual([], validate('1+2'));
     });
+
+    it("invalid chars", function () {
+        assert.deepEqual([
+            {
+                "column": 1,
+                "length": 1,
+                "line": 0,
+                "text": "token recognition error at: '_'"
+            }, {
+                "column": 2,
+                "length": 1,
+                "line": 0,
+                "text": "token recognition error at: '_'"
+            }, {
+                "column": 3,
+                "length": 1,
+                "line": 0,
+                "text": "extraneous input '2' expecting NEWLINE"
+            }
+        ], validate('1__2'));
+    });
 });
